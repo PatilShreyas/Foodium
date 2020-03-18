151aedc61d37ea6ff8bf4582d72933c5a20b2b65
Shreyas Patil <shreyaspatilg@gmail.com>
2020-03-19 00:18:22 +0530

    Added ViewModelProvider.Factory

---
+++ app/src/main/java/dev/shreyaspatil/foodium/ViewModelProviderFactory.kt
@@ -0,0 +1,27 @@
+package dev.shreyaspatil.foodium
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Suppress("UNCHECKED_CAST")
+@Singleton
+class ViewModelProviderFactory @Inject constructor(
+    private val viewModelsMap: Map<Class<out ViewModel>,
+            @JvmSuppressWildcards Provider<ViewModel>>
+) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = viewModelsMap[modelClass] ?: viewModelsMap.asIterable().firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        return try {
+            creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
