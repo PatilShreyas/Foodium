697cb5e32da5a575b2e6f9f31d4133d9d4817c2b
Shreyas Patil <shreyaspatilg@gmail.com>
2020-03-19 00:16:23 +0530

    Changes in Service

--- app/src/main/java/dev/shreyaspatil/foodium/api/FoodiumService.kt
+++ app/src/main/java/dev/shreyaspatil/foodium/api/FoodiumService.kt
@@ -1,17 +1,17 @@
 package dev.shreyaspatil.foodium.api
 
 import dev.shreyaspatil.foodium.model.Post
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
-typealias PostsResponse = Call<List<Post>>
+typealias PostsResponse = Response<List<Post>>
 
 interface FoodiumService {
 
-    @GET("/posts")
-    fun getPosts(): PostsResponse
+    @GET("/DummyFoodiumApi/api/posts/")
+    suspend fun getPosts(): PostsResponse
 
     companion object {
-        const val FOODIUM_API_URL = "https://patilshreyas.github.io/DummyFoodiumApi/api/"
+        const val FOODIUM_API_URL = "https://patilshreyas.github.io/"
     }
 }
