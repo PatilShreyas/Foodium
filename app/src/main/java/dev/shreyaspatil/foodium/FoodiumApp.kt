1346cc924c9b4b1923648c6aa1e763a13bdde3e3
Shreyas Patil <shreyaspatilg@gmail.com>
2020-06-15 19:31:13 +0530

    Added hilt annotation in Application

--- app/src/main/java/dev/shreyaspatil/foodium/FoodiumApp.kt
+++ app/src/main/java/dev/shreyaspatil/foodium/FoodiumApp.kt
@@ -26,28 +26,16 @@ package dev.shreyaspatil.foodium
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
-import dev.shreyaspatil.foodium.di.component.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 import dev.shreyaspatil.foodium.utils.isNight
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
-class FoodiumApp : Application(), HasAndroidInjector {
-
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+@HiltAndroidApp
+class FoodiumApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        // Initialize Dependency Injection
-        DaggerAppComponent.builder()
-            .create(this)
-            .build()
-            .inject(this)
 
         // Get UI mode and set
         val mode = if (isNight()) {
@@ -58,6 +46,4 @@ class FoodiumApp : Application(), HasAndroidInjector {
 
         AppCompatDelegate.setDefaultNightMode(mode)
     }
-
-    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
