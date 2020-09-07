a143fa0a5d9bfd8002f3b92ef0fd890a7a43f183
GeorgCantor <cupsman1986@gmail.com>
2020-09-07 16:52:57 +0300

    added slide animation of recyclerView with MotionLayout

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -50,7 +50,7 @@ object Android {
     const val appcompat = "androidx.appcompat:appcompat:1.1.0"
     const val activityKtx = "androidx.activity:activity-ktx:1.1.0"
     const val coreKtx = "androidx.core:core-ktx:1.2.0"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.1"
     const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
 }
 
