e753909509e5278c910a9b39cf1e8d1320cb7c91
Shreyas <shreyaspatilg@gmail.com>
2021-01-16 00:31:51 +0530

    Create interface-based post repository

--- app/src/main/java/dev/shreyaspatil/foodium/data/repository/PostRepository.kt
+++ app/src/main/java/dev/shreyaspatil/foodium/data/repository/PostRepository.kt
@@ -34,26 +34,31 @@ import retrofit2.Response
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@Singleton
+interface PostRepository {
+    fun getAllPosts(): Flow<Resource<List<Post>>>
+    fun getPostById(postId: Int): Flow<Post>
+}
+
 /**
  * Singleton repository for fetching data from remote and storing it in database
  * for offline capability. This is Single source of data.
  */
 @ExperimentalCoroutinesApi
 @Singleton
-class PostsRepository @Inject constructor(
+class DefaultPostRepository @Inject constructor(
     private val postsDao: PostsDao,
     private val foodiumService: FoodiumService
-) {
+) : PostRepository {
 
     /**
      * Fetched the posts from network and stored it in database. At the end, data from persistence
      * storage is fetched and emitted.
      */
-    fun getAllPosts(): Flow<Resource<List<Post>>> {
+    override fun getAllPosts(): Flow<Resource<List<Post>>> {
         return object : NetworkBoundRepository<List<Post>, List<Post>>() {
 
-            override suspend fun saveRemoteData(response: List<Post>) =
-                postsDao.insertPosts(response)
+            override suspend fun saveRemoteData(response: List<Post>) = postsDao.addPosts(response)
 
             override fun fetchFromLocal(): Flow<List<Post>> = postsDao.getAllPosts()
 
@@ -67,5 +72,5 @@ class PostsRepository @Inject constructor(
      * @return [Post] data fetched from the database.
      */
     @MainThread
-    fun getPostById(postId: Int): Flow<Post> = postsDao.getPostById(postId)
+    override fun getPostById(postId: Int): Flow<Post> = postsDao.getPostById(postId)
 }
