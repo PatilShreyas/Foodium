7948b899f64a94fa845d38cb6d0891d8a9affec6
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-01-23 11:07:09 +0530

    Update build tools and dependency versions (#63)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,16 +1,16 @@
 object Testing {
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.1"
-    const val room = "androidx.room:room-testing:2.2.5"
+    const val room = "androidx.room:room-testing:2.2.6"
     const val jUnit = "junit:junit:4.13"
     const val extJUnit = "androidx.test.ext:junit:1.1.1"
-    const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
-    const val okHttp = "com.squareup.okhttp3:mockwebserver:4.4.0"
+    const val espresso = "androidx.test.espresso:espresso-core:3.3.0"
+    const val okHttp = "com.squareup.okhttp3:mockwebserver:4.9.0"
     const val core = "androidx.arch.core:core-testing:2.1.0"
 }
 
 object Dependencies {
-    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.4.21"
-    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.4.21-2"
+    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21-2"
     const val gradle = "com.android.tools.build:gradle:4.1.1"
     const val daggerHilt = "com.google.dagger:hilt-android-gradle-plugin:2.31-alpha"
     const val ktLint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
@@ -32,26 +32,26 @@ object Hilt {
 }
 
 object Moshi {
-    const val moshi = "com.squareup.moshi:moshi-kotlin:1.9.2"
-    const val codeGen = "com.squareup.moshi:moshi-kotlin-codegen:1.9.2"
+    const val moshi = "com.squareup.moshi:moshi-kotlin:1.11.0"
+    const val codeGen = "com.squareup.moshi:moshi-kotlin-codegen:1.11.0"
 }
 
 object Retrofit {
-    const val retrofit = "com.squareup.retrofit2:retrofit:2.8.1"
-    const val moshiRetrofitConverter = "com.squareup.retrofit2:converter-moshi:2.7.2"
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val moshiRetrofitConverter = "com.squareup.retrofit2:converter-moshi:2.9.0"
 }
 
 object Coroutines {
-    const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1"
-    const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1"
+    const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2"
+    const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2"
 }
 
 object Android {
-    const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.2.0"
     const val activityKtx = "androidx.activity:activity-ktx:1.1.0"
-    const val coreKtx = "androidx.core:core-ktx:1.2.0"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
-    const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
+    const val coreKtx = "androidx.core:core-ktx:1.3.2"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+    const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
 }
 
 object Room {
